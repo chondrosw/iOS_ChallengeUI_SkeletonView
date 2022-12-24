4311c72a612dfa2a0614d5e1c1533a0d50a6ddfd
chondrosw <chondrosatriowibowo@gmail.com>
2022-12-25 01:49:04 +0700

    Initial Commit

---
+++ Challange1_SkeletonView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Challange1_SkeletonView
+//
+//  Created by Gramedia on 25/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
